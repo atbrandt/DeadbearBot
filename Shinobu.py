3ba188c3885e801e51b667ed668cd142f846237a
spacebear <1spacebear@gmail.com>
2019-04-17 12:23:54 -0700

    Added ability for the bot to generate a config automatically if one doesn't already exist

--- Shinobu.py
+++ Shinobu.py
@@ -8,32 +8,34 @@ import configparser
 from discord.ext import commands
 from pathlib import Path
 
-# Path variables for importing files cleanly regardless of deploy environment
-# ConfigFolder = Path("")
-# ConfigFile = ConfigFolder / "init.cfg"
+# Setting platform-independent path to working directory and config file
+CURRENTDIR = Path.cwd()
+CONFIG = CURRENTDIR / "init.cfg"
 
-# Initialize global variables and functions
-CFGPARSE = configparser.ConfigParser()
-CFGPARSE.read('init.cfg')
-bot = commands.Bot(command_prefix = CFGPARSE['DEFAULT']['Prefix'])
-greet = CFGPARSE['DEFAULT']['Greet']
-greetChannel = CFGPARSE['DEFAULT']['GreetChannel']
+# Initialize configparser settings
+CFGPARSE = configparser.ConfigParser(allow_no_value = True,
+                                     empty_lines_in_values = False)
 
-
-# Function to reinitialize the Bot
-def update_prefix():
-    if CFGPARSE['DEFAULT']['Prefix'] == "":
-        bot.command_prefix = '-'
-    else:
-        bot.command_prefix = CFGPARSE['DEFAULT']['Prefix']
-
-
-# Function for writing to cfg
-def write_cfg(category, entry, value):
-    CFGPARSE.set(category, entry, value)
-    with open('init.cfg', 'w') as configfile:
+# Function for writing to the config file
+def write_cfg(*args, **kwargs):
+    for key, value in kwargs.items():
+        CFGPARSE.set(args, key, value)
+    with Path.open(CONFIG, 'w') as configfile:
         CFGPARSE.write(configfile)
 
+# Initialize config
+if not Path.exists(CONFIG):
+    CFGPARSE['DEFAULT'] = {'Token': '',
+                           'Prefix': '-',
+                           'Greet': 'disable',
+                           'GreetChannel': ''}
+    TOKEN = input("Enter your bot's token: ")
+    write_cfg(Token=TOKEN)
+else:
+    CFGPARSE.read(CONFIG)
+
+# Initialize the bot and set the default command prefix
+bot = commands.Bot(command_prefix = CFGPARSE['DEFAULT']['Prefix'])
 
 # Output info to console once bot is initialized and ready
 @bot.event
@@ -54,7 +56,8 @@ async def hello_world(context):
 # Set the greet channel
 @bot.command(name = 'ChangeGreet',
              description = ("Sets the current channel for greet messages. "
-                            "Pass \"enable\" or \"disable\" to turn the greet message on or off."),
+                            "Pass \"enable\" or \"disable\" to turn "
+                            "the greet message on or off."),
              brief = "Modify greet message settings.",
              aliases = ["greet"])
 async def change_greet(context, *opt_arg):
@@ -62,19 +65,19 @@ async def change_greet(context, *opt_arg):
     opt_arg = ''.join(opt_arg)
     options = {'enable', 'disable'}
     
-    if opt_arg is ():
-        write_cfg('DEFAULT', 'GreetChannel', str(context.channel.id))
+    if opt_arg == '':
+        write_cfg(GreetChannel=str(context.channel.id))
         await channel.send(f"Set {channel} as the greeting channel.")
     elif opt_arg in options:
-        write_cfg('DEFAULT', 'Greet', str(opt_arg))
+        write_cfg(Greet=str(opt_arg))
         await channel.send(f"The greet message is now {opt_arg}d!")
 
 
 # Greet new users upon joining guild
 @bot.event
 async def on_member_join(member):
-    if greet == "enable":
-        channel = bot.get_channel(int(greetChannel))
+    if CFGPARSE['Greet'] == "enable":
+        channel = bot.get_channel(int(CFGPARSE['GreetChannel']))
         await channel.send(f"Welcome to the server {member.mention}!")
 
 
@@ -91,23 +94,21 @@ async def change_prefix(context):
     def check(reply):
         return reply.author == author and reply.channel == channel
 
-    await channel.send("What prefix would you like me to respond to? \[Auto timeout in 10 seconds.\]")
+    await channel.send("What prefix would you like me to respond to? "
+                       "\[Auto timeout in 10 seconds.\]")
 
     try:
         newprefix = await bot.wait_for('message', check=check, timeout=10.0)
     except asyncio.TimeoutError:
         await channel.send("Timed out! Exiting...")
     else:
-        write_cfg('DEFAULT', 'Prefix', str(newprefix.content))
-        update_prefix()
+        write_cfg(Prefix=str(newprefix.content))
+        bot.command_prefix = newprefix.content
         await channel.send(f"My command prefix is now \"{newprefix.content}\".")
 
 
 # Execute the Bot
-if CFGPARSE['DEFAULT']['Token'] == "":
-    TOKEN = input("Enter your bot's token: ")
-    write_cfg('DEFAULT', 'Token', TOKEN)
-else:
-    TOKEN = CFGPARSE['DEFAULT']['Token']
-
-bot.run(TOKEN)
+try:
+    bot.run(CFGPARSE['DEFAULT']['Token'])
+except:
+    print("Something's wrong with the token! Check the config file.")
