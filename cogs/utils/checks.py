fc8f73abc63a14d26bd7cf69a4fb8b0cf0d0e6f1
spacebear <1spacebear@gmail.com>
2020-11-29 12:52:31 -0800

    Changed perm check to return true if no permrole set

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -14,6 +14,8 @@ async def check_perm_role(ctx):
     if ctx.author.id == ctx.bot.owner_id:
         return True
     permrole = await db.get_cfg(ctx.guild.id, 'perm_role')
+    if not permrole:
+        return True
     gotRole = ctx.guild.get_role(permrole)
     roles = ctx.author.roles
     return gotRole in roles
